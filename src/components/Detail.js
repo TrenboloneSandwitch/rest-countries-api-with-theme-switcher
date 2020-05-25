281ebf1e45f19d9d4c60fe133a69cff2faed88cf
Bobby Sixkiller <soldier068@email.cz>
2020-05-25 21:24:31 +0200

    reworked detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams, Link } from "@reach/router";
 import axios from "axios";
 import { endpoint } from "../config";
+import { ThemeContext } from "../context/ThemeContext";
 
 const Detail = () => {
   const { alpha3Code } = useParams();
   const [country, setCountry] = useState(null);
+  const { state: theme } = useContext(ThemeContext);
 
   useEffect(() => {
     async function getCountry() {
@@ -18,54 +20,74 @@ const Detail = () => {
 
   return (
     <div className="container">
-      <Link to="/">BACK</Link>
+      <Link
+        to="/"
+        className={`button mt-4 element-bg--${theme.isDark ? "dark" : "light"}`}
+      >
+        <i className="fa fa-arrow-left" /> BACK
+      </Link>
       {!country ? (
         <p>Loading</p>
       ) : (
         <React.Fragment>
           <div className="detail-container">
-            <img src={country.flag} alt={`Flag of the ${country.name}`} />
-            <div className="information">
+            <div className="img-cont mt-4">
+              <img src={country.flag} alt={`Flag of the ${country.name}`} />
+            </div>
+            <div className="information mt-4">
               <h1> {country.name}</h1>
-              <ul className="cl">
-                <li>
-                  <span className="bold">Native Name: </span>
-                  {country.nativeName}
-                </li>
-                <li>
-                  <span className="bold">Population: </span>
-                  {country.population.toLocaleString("en")}
-                </li>
-                <li>
-                  <span className="bold">Region: </span>
-                  {country.region}
-                </li>
-                <li>
-                  <span className="bold">Sub Region: </span>
-                  {country.subregion}
-                </li>
-                <li>
-                  <span className="bold">Capital: </span>
-                  {country.capital}
-                </li>
-              </ul>
-              <ul className="cl">
-                <li>
-                  <span className="bold">Top Level Domain: </span>
-                  {country.topLevelDomain.map((domain) => domain)}
-                </li>
-                <li>
-                  <span className="bold">Currencies: </span>
-                  {country.currencies.map((currency) => currency.name + ", ")}
-                </li>
-                <li>
-                  <span className="bold">Languages: </span>
-                  {country.languages.map((language) => language.name + ", ")}
-                </li>
-              </ul>
-              <div className="borders">
+              <div className="space-between align-top">
+                <ul className="col information__list">
+                  <li>
+                    <span className="bold">Native Name: </span>
+                    {country.nativeName}
+                  </li>
+                  <li>
+                    <span className="bold">Population: </span>
+                    {country.population.toLocaleString("en")}
+                  </li>
+                  <li>
+                    <span className="bold">Region: </span>
+                    {country.region}
+                  </li>
+                  <li>
+                    <span className="bold">Sub Region: </span>
+                    {country.subregion}
+                  </li>
+                  <li>
+                    <span className="bold">Capital: </span>
+                    {country.capital}
+                  </li>
+                </ul>
+                <ul className="col information__list">
+                  <li>
+                    <span className="bold">Top Level Domain: </span>
+                    {country.topLevelDomain.map((domain) => domain)}
+                  </li>
+                  <li>
+                    <span className="bold">Currencies: </span>
+                    {country.currencies.map((currency) => currency.name + ", ")}
+                  </li>
+                  <li>
+                    <span className="bold">Languages: </span>
+                    {country.languages.map((language) => language.name + ", ")}
+                  </li>
+                </ul>
+              </div>
+              <div className="borders mt-4">
                 <span className="bold">Border Countries: </span>
-                {country.borders.map((border) => border + " ")}
+                <div className="borders__list">
+                  {country.borders.map((border) => (
+                    <span
+                      key={border}
+                      className={`badge element-bg--${
+                        theme.isDark ? "dark" : "light"
+                      }`}
+                    >
+                      {border}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
